14505a70c065fba7e93e451a0cd0308479b14378
shauru1118 <sargsyan_ae09@mail.ru>
2025-10-03 14:10:50 +0300

    %1

--- .sh
+++ .sh
@@ -23,7 +23,7 @@ sudo docker build . -t shauru1118/new-tgbot
 echo =
 echo Docker : build DONE
 echo =
-sudo docker push shauru1118/new-tgbot
+sudo docker push shauru1118/new-tgbot:latest
 echo =
 echo Docker : push DONE
 echo =
